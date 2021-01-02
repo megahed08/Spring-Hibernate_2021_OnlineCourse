2b857117cc0ed938ab8b1fcfa6910d6f956562fe
megahed08 <megahedy08@gmail.com>
2021-01-02 23:50:23 +0100

    Up till Constructor Injection

---
+++ spring-demo-one/src/com/luv2code/springdemo/QuidditchCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class QuidditchCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "try to beat the Three River Dragons of Passau";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
