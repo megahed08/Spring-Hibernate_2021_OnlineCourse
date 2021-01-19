12cdb27ac8331b3b60a214c98c493e718f7c4fa9
megahed08 <megahedy08@gmail.com>
2021-01-19 21:52:28 +0100

    Setter injection

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -11,11 +11,24 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	//define a default constructor
+	public TennisCoach() {
+		System.out.println(">>TennisCoach: inside default constuctor");	
+	}
+	
+	//define a setter method
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">>TennisCoach: inside setFortuneService() method");	
+		fortuneService = theFortuneService;
+	}
+	/*
 	//if @Autowired is commented it will still inject the constructor as of new Spring feature
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	@Override
 	public String getDailyWorkout() {
